c899a0555ac2214342ba2b4da2c6baefebfdc25e
Purbayan <purbayan.00@gmail.com>
2026-07-12 10:37:55 +0530

    Refactor power script for session management

--- config_dotfiles/config/custom_scripts/power.sh
+++ config_dotfiles/config/custom_scripts/power.sh
@@ -1,30 +1,12 @@
 #!/usr/bin/env bash
-
-# Detect whether we're running on Xorg or Wayland
-if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
-    suspend="systemctl suspend && swaylock -i ~/.config/walls/wall.png"
-else
-    suspend="systemctl suspend && i3lock -i ~/.config/walls/wall.png"
-fi
-
-# special method for loggin out :(
-if [ "$XDG_CURRENT_DESKTOP" = "i3" ]; then
-    logout="killall i3"
-elif [ "$XDG_CURRENT_DESKTOP" = "Hyprland" ]; then
-    logout="killall Hyprland"
-elif [ "$XDG_CURRENT_DESKTOP" = "sway:wlroots" ]; then
-    logout="killall sway"
-else
-  logout="loginctl terminate-user $USER"
-fi
-
-chosen=$(printf "Log Out\nSuspend\nRestart\nPower OFF\n" | rofi -dmenu -i -theme-str '@import "~/.config/rofi/themes/powermenu.rasi"')
-
-# Perform the action based on user choice
+lock="swaylock -f -i $HOME/.config/walls/wall.png"
+if pgrep -x sway >/dev/null; then logout="swaymsg exit"; else logout="loginctl terminate-user $USER"; fi
+chosen=$(printf "Log Out\nLock\nSuspend\nRestart\nPower OFF\n" | wofi --dmenu --prompt "power" --width 260 --lines 6)
 case "$chosen" in
-    "Log Out") eval "$logout" ;;
-    "Suspend") eval "$suspend" ;;
-    "Restart") systemctl reboot ;;
-    "Power OFF") systemctl poweroff ;;
-    *) exit 1 ;;
+ "Log Out")   eval "$logout" ;;
+ "Lock")      eval "$lock" ;;
+ "Suspend")   eval "$lock"; loginctl suspend ;;
+ "Restart")   loginctl reboot ;;
+ "Power OFF") loginctl poweroff ;;
+ *) exit 1 ;;
 esac
