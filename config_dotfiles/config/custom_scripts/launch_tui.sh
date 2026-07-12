12d367dab7685c33d18e5be83ba25bc70b835231
Purbayan <purbayan.00@gmail.com>
2026-07-12 18:02:07 +0530

    Delete config_dotfiles directory

--- config_dotfiles/config/custom_scripts/launch_tui.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -u
-(($#)) || { notify-send "Terminal launcher" "No command supplied"; exit 1; }
-command -v "$1" >/dev/null || { notify-send "Missing command" "$1"; exit 1; }
-footclient -e "$@" >/dev/null 2>&1 &
--- config_dotfiles/config/custom_scripts/wlsunset.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-if pgrep -x wlsunset >/dev/null; then pkill -x wlsunset; else wlsunset -T 4001 >/dev/null 2>&1 & fi
